58892a7c3211e9ade735943d35df655c505c556e
Charles Prystupa <Charles.Prystupa@gmail.com>
2021-01-04 13:06:13 -0500

    Rough logic: 1 Player Game

--- main.rb
+++ main.rb
@@ -1,35 +1,59 @@
 # frozen_string_literal: true
 
 class Game
-  def gen_numbers
+  def question(current_player)
     num1 = rand(1..20)
     num2 = rand(1..20)
-  end
 
-  def question
-    @question = puts "#{current_player}: What does #{num1} plus #{num2} equal?"
+    @question = puts "#{current_player.name}: What does #{num1} plus #{num2} equal?"
+    answer = $stdin.gets.chomp
+
+    # Check answer
+    if answer == (num1 + num2).to_s
+      puts 'That is correct!'
+    else
+      puts "Nope... the answer is #{num1 + num2}"
+      # decrement a life
+      current_player.lives -= 1
+    end
   end
 
   def turn
     puts '----- NEW TURN -----'
-    @question
   end
+end
+
+class Question
 end
 
 class Player
+  attr_accessor :lives
+  attr_reader :name
+
   def initialize(name)
     @name = name
+    @lives = 3
+  end
+
+  def info
+    "#{name}: #{lives}/3"
   end
 end
 
-# Question
+puts 'Want to play a game...Two Players Math!!!'
+# Initialize Players
+p1 = Player.new('Player 1')
+p2 = Player.new('Player 2')
 
-answer = gets.chomp
-# Check answer
-if answer == (num1 + num2)
-  puts 'That is correct!'
-else
-  puts "Nope... the answer is #{num1 + num2}"
-  # Score
-  puts "P1: #{p1_lives}/3 vs P2: #{p2_lives}/3"
+current_player = p1
+
+game = Game.new
+
+while p1.lives > 0
+  game.question(current_player)
+  # score
+  puts "#{p1.info} VS #{p2.info}"
+  if p1.lives == 0 
+    puts "Good game, goodbye"
+  end
 end
